2a5bc77a11b9fd9c316a66807ed92cdabd62c5b4
miroslawszyper <miroslawszyper@gmail.com>
2023-02-20 10:56:37 +0100

    Add new files

---
+++ 1_podstawy/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _1_podstawy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 45;
+            Console.WriteLine(x);
+            Console.Clear();
+
+            string t = "";
+            t = x.ToString();
+            Console.WriteLine(t);
+
+            int y = int.Parse(t);
+            Console.WriteLine(y);
+            double a = double.Parse(t);
+            Console.WriteLine(a);
+
+            Console.WriteLine(a.GetType()); //System.Double
+            Console.WriteLine(sizeof(double)); //8 bajtów
+            Console.WriteLine(sizeof(float)); //4 bajtów
+
+
+        }
+    }
+}
