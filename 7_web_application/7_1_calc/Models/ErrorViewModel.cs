5b6cb61fcfdfff95e022a190826979f65b56f432
szyper <zskgit@gmail.com>
2023-04-24 12:09:55 +0200

    calculator

---
+++ 7_web_application/7_1_calc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _7_1_calc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
